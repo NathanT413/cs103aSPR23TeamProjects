5eccb4b64459f8f7affe937b8a47b54da24c2780
AG3NTZ3R0 <brandon.lacy.50@gmail.com>
2023-03-04 16:37:09 -0500

    feat: complete the gpt_code_docs and gpt_code_docs_about

--- ca01/gptwebapp.py
+++ ca01/gptwebapp.py
@@ -38,9 +38,11 @@ def index():
 		<h1>GPT Demo</h1>
 		<h2><a href="{url_for('gptdemo')}">Ask questions to GPT</a></h2>
 		<br>
+		<h2><a href="{url_for('about')}">About</a><h2>
+		<br>
 		<h2><a href="{url_for('team')}">Team</a></h2>
 		<br>
-		<h2><a href="{url_for('code_docs')}'>Code Documentation</a></h2>
+		<h2><a href="{url_for('gpt_code_docs')}'>Code Documentation</a></h2>
 		<br>
 	 '''
 
@@ -74,12 +76,26 @@ def gptdemo():
 			</form>
 		'''
 
+
+@app.route('/about')
+def about():
+	'''
+	A page to describe the intent of gptwebapp.py.
+	'''
+	return '''
+		<h1>About</h1>
+		<h2>Description</h2>
+		<p></p>
+	'''
+
+
 @app.route('/team')
 def team():
 	'''
 	A page with a short biography about each team member.
+	Brandon J. Lacy (AG3NTZ3R0)
 	'''
-	return '''
+	return f'''
 		<h1>Team</h1>
 		<h2>Brandon J. Lacy</h2>
 		<h3>Biography</h3>
@@ -87,8 +103,8 @@ def team():
 incident response team for the Office of the Comptroller of the Commonwealth of Massachusetts and as a member of the data science and insights team for Hubbell Inc. Brandon is excited to expand his knowledge of machine learning 
 in an academmic setting to allow him to gain a deeper understand of the technology from both an application and mathematical perspective.</p>
 		<h3>Role</h3>
-		<p>Brandon was responsible for the the creation of the ca01 folder, the duplication of the gptwebapp.py and gpt.py files from lesson15, the creation of the /code_docs route in gptwebapp.py and code_docs method in 
-gpt.py, the creation of the team page, and his personal page.</p>
+		<p>Brandon was responsible for the the creation of the ca01 folder, the duplication of the gptwebapp.py and gpt.py files from lesson15, the creation of the <a href={url_for('gpt_code_docs')}>/gpt_code_docs</a> and 
+<a href={url_for('gpt_code_docs_about')}>/gpt_code_docs/about</a> routes in gptwebapp.py and code_docs method in gpt.py, the creation of the team page, the creation of the about page, and his personal page.</p>
 	'''
 
 
@@ -104,12 +120,15 @@ def gpt_code_docs():
 		answer = gptAPI.code_docs(lang, code)
 		return f'''
 			<h1>ChatGPT Code Documentation</h1>
+			<h2><a href={url_for('gpt_code_docs_about')}>About</a></h2>
 			<h2>Response</h2>
 			<pre style="border:thin solid black">{answer}</pre>
+			<h2><a href={url_for('index')}>Index</a></h2>
 		'''
 	else:
-		return '''
+		return f'''
 			<h1>ChatGPT Code Documentation</h1>
+			<h2><a href={url_for('gpt_code_docs_about')}>About</a></h2>
 			<h2>Query</h2>
 			<form method="post">
 				<h3><label for="lang">Programming Language</label></h3>
@@ -120,7 +139,26 @@ def gpt_code_docs():
 				<br>
 				<input type=submit>
 			</form>
+			<h2><a href={url_for('index')}>Index</a></h2>
 		'''
+
+
+@app.route('/code_docs/about')
+def gpt_code_docs_about():
+	'''
+	The about page that corresponds with gpt_code_docs and explains the intent as well as the way to operate the route.
+	Brandon J. Lacy (AG3NTZ3R0)
+	'''
+	return f'''
+		<h1>About Code Documentation</h1>
+		<h2>Description</h2>
+		<p>The purpose of this route is to provide a programmer with the ease of convenience in code documentation. A majority of the time a programmer documents their code haphazardly as they believe there is more important 
+work to be completed, which leads to trouble down the road when maintenance needs to be performed on an application. Though we do not condone haphazard code documentation and recommend the individual to dedicate time to this 
+critical development practice, we want to avoid the pain for the developer down the road that may need to interact with code with lackluster documentation.</p>
+		<h2>Instructions</h2>
+		<p>The route is incredibly simple to use, as we understand your lack of time on unnecessary tasks. Simply enter the programming language of the code and the code in their respective text boxes.</p>
+		<h2><a href={url_for('gpt_code_docs')}>Back to Code Documentation</a></h2>
+	'''
 
 
 if __name__=='__main__':
