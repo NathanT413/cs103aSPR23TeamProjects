1b392bb62bedb9e925f4001f41b09bd3791ca0c6
AG3NTZ3R0 <Z3R0@duck.com>
2023-03-24 20:03:54 -0400

    feat: code infrastructure implemented

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -3,27 +3,38 @@ An application that tracks finances with persistence with through the utilizatio
 
 Author: Brandon J. Lacy (AG3NTZ3R0)
 '''
-
-
 import sys
 
+from functools import partial
 from transaction import Transaction
+
 
 # Author: Brandon J. Lacy (AG3NTZ3R0)
 # Store the arguments from the command line execution of the script
 # No arguments so prompt the user for them
 if len(sys.argv) == 1:
     arg = input('cmd > ').split(' ')
-# Numerous arguments so prompt the user to select one
-elif len(sys.argv) > 2:
-    print("Only one argument at a time.")
-
-    args = [(i, sys.argv[1:][i]) for i in range(len(sys.argv[1:]))]
-    for arg in args: print(f"{arg[0]}: {arg[1]}")
-
-    choice = int(input("Which argument should be executed? "))
-    arg = sys.argv[1:][choice]
 else:
     arg = sys.argv[1]
+    # Store the values for the option
+    vals = tuple(val for val in sys.argv[2:])
 
-    
+# Author: Brandon J. Lacy (AG3NTZ3R0)
+trans_orm = Transaction('tracker.db')
+# Act upon the option specified by the user (TEAM: REPLACE "" WITH METHOD AS SEEN BELOW) 
+options = {
+    '--help': "",
+    '--show-trans': partial(trans_orm.selectAll),
+    '--add-trans': partial(trans_orm.add),
+    '--del-trans': "",
+    '--sum-trans-d': "",
+    '--sum-trans-m': "",
+    '--sum-trans-y': "",
+    '--sum-trans-c': ""
+}
+
+try:
+    print(options[arg](vals))
+except Exception as e:
+    print("Unavailable option specified.")
+    print(e)
