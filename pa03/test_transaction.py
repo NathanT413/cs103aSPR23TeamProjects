c4420ae88eed1c8ce7b02af46e80d3d07b8bfd82
Eric Wang <ericwang223@brandeis.edu>
2023-04-11 23:37:42 -0400

    added to test_transaction.py

--- pa03/test_transaction.py
+++ pa03/test_transaction.py
@@ -11,11 +11,15 @@ def example_data():
     '''
     A few data entries to assist in the test of application functionality.
 
-    Author: Brandon J. Lacy (AG3NTZ3R0)
+    Author: Brandon J. Lacy (AG3NTZ3R0), Eric Wang
     '''
     return [
         (1, 9.99, 'food', '2023-03-23', 'fast-food'),
-        (2, 99.99, 'food', '2023-03-28', 'groceries')
+        (2, 99.99, 'food', '2023-03-28', 'groceries'),
+        (4, 12.99, 'McDonalds', '2023-03-23', 'food'),
+        (5, 120450.99, 'MercedesACLass', '2019-04-23', 'Vehical'),
+        (6, 5040.0, 'MacbookPro', '2019-05-19', 'computer'),
+        (7, 1250.0, 'Ipad', '2019-05-19', 'tablet')
     ]
 
 
@@ -103,3 +107,131 @@ def test_add(transaction_db, example_data_dict):
     results = trans_orm.select_all(())
 
     assert results == updated_example_data_dict
+
+def test_del(transaction_db, example_data_dict):
+    '''
+    Test the ability to delete a transaction from the database.
+    
+    Author: Eric Wang
+    '''
+    trans_orm = transaction_db
+
+    tup = (1, 9.99, 'food', '2023-03-23', 'fast-food')
+    updated_example_data_dict = example_data_dict
+    updated_example_data_dict.remove({
+        'id': tup[0],
+        'amount': tup[1],
+        'category': tup[2],
+        'date': tup[3],
+        'description': tup[4]
+    })
+
+    trans_orm.delete(str(tup[0]))
+
+    results = trans_orm.select_all(())
+
+    assert results == updated_example_data_dict
+
+def test_sum_date(transaction_db):
+    '''
+    Test the ability to find transaction given the date.
+    
+    Author: Eric Wang
+    '''
+    trans_orm = transaction_db
+    tup1 = (1, 9.99, 'food', '2023-03-23', 'fast-food')
+    tup2 = (4, 12.99, 'McDonalds', '2023-03-23', 'food')
+    expect = []
+    expect.append({
+        'id': tup1[0],
+        'amount': tup1[1],
+        'category': tup1[2],
+        'date': tup1[3],
+        'description': tup1[4]
+    })
+    expect.append({
+        'id': tup2[0],
+        'amount': tup2[1],
+        'category': tup2[2],
+        'date': tup2[3],
+        'description': tup2[4]
+    })
+
+    results = trans_orm.sum_date(['2023-03-23'])
+
+    assert results == expect
+
+def test_sum_month(transaction_db):
+    '''
+    Test the ability to find transaction given the year and month.
+    
+    Author: Eric Wang
+    '''
+    trans_orm = transaction_db
+    tup1 = (1, 9.99, 'food', '2023-03-23', 'fast-food')
+    tup2 = (4, 12.99, 'McDonalds', '2023-03-23', 'food')
+    tup3 = (2, 99.99, 'food', '2023-03-28', 'groceries')
+    expect = []
+    expect.append({
+        'id': tup3[0],
+        'amount': tup3[1],
+        'category': tup3[2],
+        'date': tup3[3],
+        'description': tup3[4]
+    })
+    expect.append({
+        'id': tup1[0],
+        'amount': tup1[1],
+        'category': tup1[2],
+        'date': tup1[3],
+        'description': tup1[4]
+    })
+    expect.append({
+        'id': tup2[0],
+        'amount': tup2[1],
+        'category': tup2[2],
+        'date': tup2[3],
+        'description': tup2[4]
+    })
+   
+    results = trans_orm.sum_month(['2023-03'])
+
+    assert results == expect
+
+    def test_sum_year(transaction_db):
+        '''
+        Test the ability to find transaction given the year.
+        
+        Author: Eric Wang
+        '''
+        trans_orm = transaction_db
+
+        tup2 = (5, 120450.99, 'MercedesACLass', '2019-04-23', 'Vehical')
+        tup3 =(6, 5040.0, 'MacbookPro', '2019-05-19', 'computer')
+        tup1 = (7, 1250.0, 'Ipad', '2019-05-19', 'tablet')
+        expect = []
+        expect.append({
+            'id': tup3[0],
+            'amount': tup3[1],
+            'category': tup3[2],
+            'date': tup3[3],
+            'description': tup3[4]
+        })
+        expect.append({
+            'id': tup1[0],
+            'amount': tup1[1],
+            'category': tup1[2],
+            'date': tup1[3],
+            'description': tup1[4]
+        })
+        expect.append({
+            'id': tup2[0],
+            'amount': tup2[1],
+            'category': tup2[2],
+            'date': tup2[3],
+            'description': tup2[4]
+        })
+    
+        results = trans_orm.sum_month(['2019'])
+
+        assert results == expect
