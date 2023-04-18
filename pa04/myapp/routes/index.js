2f53b29b52f117efbb7dea73519966ca60c217a9
AG3NTZ3R0 <Z3R0@duck.com>
2023-04-18 15:46:33 -0400

    feat: connect to mongodb with mongoose

--- pa04/myapp/routes/index.js
+++ pa04/myapp/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+var Schema = mongoose.Schema;
+
+var transactionSchema = Schema({
+  description: String,
+  amount: Number,
+  category: String,
+  date: Date
+});
+
+var transactionItem = mongoose.model('Transactionitem', transactionSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Transaction Application' });
+  var transactions = transactionItem.find();
+
+  res.render('index', {
+    title: 'Transaction Application',
+    transaction: transactions 
+  });
 });
+
+router.post('/', function(req, res, next) {
+  var transaction = new transactionItem({
+    description: req.body.description,
+    category: req.body.category,
+    amount: req.body.amount,
+    date: req.body.date
+  });
+  transaction.save();
+  res.redirect('/');
+});
+
 
 module.exports = router;
